b7903d947e61f0a61442a9e91a2072b8bdcf0e30
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-09-26 17:26:08 +0500

    fead: add task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      if (knowsProgramming) return Math.ceil(800/config[focus]);
+      return Math.ceil(1300/config[focus]);
   };
-  
